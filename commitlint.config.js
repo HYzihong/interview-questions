14cc6dca282c6dc422a4d068d22c332bc5b26e33
hy <zihong2020@foxmail.com>
2022-03-11 00:07:19 +0800

    chore(deps): add git-cz changelog

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+/**
+ * @Author: hy
+ * @Date: 2022-02-09 23:14:58
+ * @LastEditors: hy
+ * @Description:
+ * @LastEditTime: 2022-03-11 00:03:41
+ * @FilePath: /interview-questions/commitlint.config.js
+ * @Copyright 2022 hy, All Rights Reserved.
+ * @仅供学习使用~
+ **/
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+};
